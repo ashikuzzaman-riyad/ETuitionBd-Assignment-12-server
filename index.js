eb711e473fffc694019e8fd92e0a2aa1ed422956
riyadhossen74 <riyadhossen696969@gmail.com>
2025-12-09 22:28:45 +0600

    added tuition post

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const express = require('express')
-const app = express()
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const uri = `mongodb+srv://${process.env.DbUser}:${process.env.DbPass}@cluster0.cymbxlh.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const db = client.db("e-tuition");
+    const tuitionCollection = db.collection("tuition");
+
+   app.get("/tuition", async (req, res) => {
+ 
+});
+
+  // get email data 
+   
+
+// post tuition
+    app.post("/new-tuitions", async (req, res) => {
+      const tuition = req.body;
+      const result = await tuitionCollection.insertOne(tuition)
+      res.send(result)
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
