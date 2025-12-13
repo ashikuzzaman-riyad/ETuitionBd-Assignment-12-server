34923091b537216b70622b858db682e0ac07d800
riyadhossen74 <riyadhossen696969@gmail.com>
2025-12-13 18:19:19 +0600

    added a ongoing statust

--- index.js
+++ index.js
@@ -181,7 +181,7 @@ async function run() {
       res.send(result);
     });
 
-    //  tuitions data get apis
+    //  tuitions data get api
     app.get("/new-tuitions", verifyFBToken, async (req, res) => {
       const query = {};
       const { email, status } = req.query;
